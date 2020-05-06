90f3f4d07c3944d8788d3a0ecb5f06c974807090
Chirag Madlani <chiragmadlani7695@gmail.com>
2020-05-06 19:10:55 +0530

    add build version

---
+++ docs/precache-manifest.76e2d2c3ef68be9b236d5bd573536500.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fc2b4b0a454f86c13f8589c163a997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76137bc49faf79234e13",
+    "url": "/static/css/main.c048af48.chunk.css"
+  },
+  {
+    "revision": "bddd7d61e0a66b1d6902",
+    "url": "/static/js/2.2782810c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2782810c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76137bc49faf79234e13",
+    "url": "/static/js/main.c6938227.chunk.js"
+  },
+  {
+    "revision": "7abde0ac5263f0bdbf7d",
+    "url": "/static/js/runtime-main.b98fa3ac.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);

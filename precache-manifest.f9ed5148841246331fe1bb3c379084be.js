18a2ea96cc405c1b3b2b40cfe6dd32fad7161cb1
chirag-madlani <chiragmadlani@ymail.com>
2020-08-14 11:47:14 +0000

    Deploying to gh-pages from  @ 42ca76b17dd9252432815467d798aa5108ae9962 🚀

---
+++ precache-manifest.f9ed5148841246331fe1bb3c379084be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845f7aa0fb53d6f34c02b0c882461eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65549fa0b8a9eb071e3",
+    "url": "/static/css/main.c048af48.chunk.css"
+  },
+  {
+    "revision": "1ea26db5101f4f748e05",
+    "url": "/static/js/2.c0fee26b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c0fee26b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65549fa0b8a9eb071e3",
+    "url": "/static/js/main.7f43b5ee.chunk.js"
+  },
+  {
+    "revision": "7abde0ac5263f0bdbf7d",
+    "url": "/static/js/runtime-main.b98fa3ac.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
